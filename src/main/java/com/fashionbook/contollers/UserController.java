86b60fdeb88199425f8f77d9d7a772631488b32a
zavlagas <zavlagas@hotmail.com>
2021-01-25 23:01:41 +0200

    I Cha

--- src/main/java/com/fashionbook/contollers/UserController.java
+++ src/main/java/com/fashionbook/contollers/UserController.java
@@ -4,6 +4,7 @@ package com.fashionbook.contollers;
 import com.fashionbook.entities.MyUser;
 import com.fashionbook.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(origins = "http://localhost:3000/")
 public class UserController {
 
     @Autowired
